336ec56bc9996e52250b554e9d0898c3c846b039
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-05-16 13:47:29 -0400

    add mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const initDB = require("./db");
 
+const init = async () => {
+  await initDB();
+  console.log("Database initialized");
+};
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+init()
+  .then(() => {
+    app.listen(3001, () => {
+      console.log("Express application started on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error during initialization:", error);
+  });
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
-});
-
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
 });
