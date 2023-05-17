5e26982f84c3dfbed36930706a2c89f5503c49dc
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-05-17 11:14:35 -0400

    add information

--- backend/fetch.py
+++ backend/fetch.py
@@ -1,4 +1,5 @@
 import feedparser
+from functools import reduce
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
 import pprint
@@ -12,10 +13,11 @@ db = client["vc_news"]  # Name of the database
 
 
 def fetch():
-    fetch_nyt()
-    fetch_wsj()
-    fetch_forbes()
-    fetch_wp()
+    # fetch_nyt()
+    # fetch_wsj()
+    # fetch_forbes()
+    # fetch_wp()
+    fetch_information()
 
 
 def fetch_nyt():
@@ -137,6 +139,35 @@ def fetch_wp():
                 "pub_date": pub_date,
             }
             collection.insert_one(article)
+
+
+def fetch_information():
+    collection = db["information"]  # Name of the collection
+    url = "https://www.theinformation.com/feed"
+    feed = feedparser.parse(url)
+
+    for entry in feed.entries:
+        title = entry.title
+        if collection.find_one({"title": title}):
+            continue
+
+        link = entry.link
+        soup = BeautifulSoup(entry.summary, "html.parser")
+        description = "".join(
+            [htag.text.strip() for htag in soup.find_all("p")]
+        ).strip()
+        authors = [author["name"] for author in entry.authors]
+        tags = []
+        pub_date = entry.published
+        article = {
+            "title": title,
+            "link": link,
+            "authors": authors,
+            "tags": tags,
+            "description": description,
+            "pub_date": pub_date,
+        }
+        collection.insert_one(article)
 
 
 fetch()
