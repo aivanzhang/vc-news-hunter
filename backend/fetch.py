30db8b6c7b9ea2bae3693852ec6c6133d5c1641b
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-05-16 22:35:40 -0400

    add wash post

--- backend/fetch.py
+++ backend/fetch.py
@@ -1,5 +1,6 @@
 import feedparser
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 import pprint
 import schedule
 import time
@@ -13,6 +14,8 @@ db = client["vc_news"]  # Name of the database
 def fetch():
     fetch_nyt()
     fetch_wsj()
+    fetch_forbes()
+    fetch_wp()
 
 
 def fetch_nyt():
@@ -27,13 +30,13 @@ def fetch_nyt():
 
         link = entry.link
         description = entry.description
-        author = entry.author
+        authors = [author.strip() for author in entry.author.split(",")]
         tags = [tag.term for tag in entry.tags]
         pub_date = entry.published
         article = {
             "title": title,
             "link": link,
-            "author": author,
+            "authors": authors,
             "tags": tags,
             "description": description,
             "pub_date": pub_date,
@@ -60,12 +63,12 @@ def fetch_wsj():
             link = entry.link
             description = entry.summary
             tags = [tag]
-            author = ""
+            authors = ""
             pub_date = entry.published
             article = {
                 "title": title,
                 "link": link,
-                "author": author,
+                "authors": authors,
                 "tags": tags,
                 "description": description,
                 "pub_date": pub_date,
@@ -73,8 +76,73 @@ def fetch_wsj():
             collection.insert_one(article)
 
 
+def fetch_forbes():
+    collection = db["forbes"]  # Name of the collection
+    url = "https://rss.app/feeds/gPewJbxDeu3FskgJ.xml"
+    feed = feedparser.parse(url)
+
+    for entry in feed.entries:
+        title = entry.title
+        if collection.find_one({"title": title}):
+            continue
+
+        link = entry.link
+        soup = BeautifulSoup(entry.summary, "html.parser")
+        description = soup.find("div").find("div").text.strip()
+        authors = [author.strip() for author in entry.author.split(",")]
+        tags = []
+        pub_date = entry.published
+        article = {
+            "title": title,
+            "link": link,
+            "authors": authors,
+            "tags": tags,
+            "description": description,
+            "pub_date": pub_date,
+        }
+        collection.insert_one(article)
+
+
+def fetch_wp():
+    collection = db["wp"]  # Name of the collection
+    url = [
+        (
+            "Technology",
+            "https://feeds.washingtonpost.com/rss/business/technology?itid=lk_inline_manual_31",
+        ),
+        (
+            "Business",
+            "https://feeds.washingtonpost.com/rss/business?itid=lk_inline_manual_37",
+        ),
+    ]
+    for tag, url in url:
+        feed = feedparser.parse(url)
+
+        for entry in feed.entries:
+            title = entry.title
+            if collection.find_one({"title": title}):
+                continue
+
+            link = entry.link
+            description = entry.summary
+            authors = [author.strip() for author in entry.author.split(",")]
+            tags = [tag]
+            pub_date = entry.published
+            article = {
+                "title": title,
+                "link": link,
+                "authors": authors,
+                "tags": tags,
+                "description": description,
+                "pub_date": pub_date,
+            }
+            collection.insert_one(article)
+
+
+fetch()
 # fetch_nyt()
-fetch_wsj()
+# fetch_wsj()
+# fetch_forbes()
 # Schedule the script to run every minute
 # schedule.every(1).minutes.do(fetch_and_store_data)
 # while True:
