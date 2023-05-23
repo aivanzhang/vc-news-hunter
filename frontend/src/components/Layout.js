8a5b7623d9b256c44f668801905fcb2f02f93d7b
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-05-23 16:36:12 -0400

    rm extra import

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Heading } from "@chakra-ui/react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Layout({ children, backButton }) {
   const navigate = useNavigate();
